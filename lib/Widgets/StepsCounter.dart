7f211cb6e31bab8b73bf3f997dcbec485548867d
bilalzaman000 <bilalzaman0102@gmail.com>
2024-06-10 20:28:15 +0500

    Step Counter Setting

--- lib/Widgets/StepsCounter.dart
+++ lib/Widgets/StepsCounter.dart
@@ -1,18 +1,28 @@
 import 'dart:math';
 
 class StepCounter {
-  static const double stepThreshold = 10.0;
+  static const double stepThreshold = 13.0;
+  static const int minTimeBetweenSteps = 250;
+
   double _previousAccMagnitude = 0.0;
   bool _isPositivePeak = false;
   int _steps = 0;
+  int _lastStepTime = 0;
 
   int get steps => _steps;
 
   void onAccelerometerEvent(double x, double y, double z) {
     double accMagnitude = sqrt(x * x + y * y + z * z);
-
+    int currentTime = DateTime.now().millisecondsSinceEpoch;
     if (_isPositivePeak && accMagnitude < _previousAccMagnitude) {
+      if (_isPositivePeak && accMagnitude < _previousAccMagnitude && (currentTime - _lastStepTime) > minTimeBetweenSteps) {
+        _steps++;
+        _lastStepTime = currentTime;
+      }
+
+      _isPositivePeak = accMagnitude > stepThreshold && accMagnitude > _previousAccMagnitude;
       _steps++;
+      _lastStepTime = currentTime;
     }
 
     _isPositivePeak = accMagnitude > stepThreshold && accMagnitude > _previousAccMagnitude;
@@ -21,5 +31,6 @@ class StepCounter {
 
   void reset() {
     _steps = 0;
+    _lastStepTime = 0;
   }
 }
